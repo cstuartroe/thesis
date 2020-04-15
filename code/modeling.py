a02c18c76a27c519689371f1c40149815cbb2570
Conor Stuart Roe <cstuartroe@haverford.edu>
2020-04-15 01:11:43 -0400

    final data generation tweaks

---
+++ code/modeling.py
@@ -0,0 +1,70 @@
+import sys
+import os
+
+from crosslingual_inflection_baseline.src import train, sigmorphon19_task1_decode
+
+language_groups = [
+    ["turkish", "bashkir", "crimean-tatar", "uzbek"],
+    ["spanish", "portuguese", "italian", "romanian"],
+    ["finnish", "estonian"],
+    ["georgian"],
+    ["navajo"],
+    ["arabic", "hebrew"],
+    ["danish", "swedish"],
+    ["czech", "slovak"],
+    ["quechua"],
+    ["zulu"],
+    ["basque"]
+]
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+def get_target_languages(source_language):
+    if source_language is None:
+        yield from flatten(language_groups)
+
+    else:
+        source_group_index = None
+        source_language_index = None
+        source_group_size = None
+        for group_index, group in enumerate(language_groups):
+            if source_language in group:
+                source_group_index = group_index
+                source_language_index = group.index(source_language)
+                source_group_size = len(group)
+
+        for group_index, group in enumerate(language_groups):
+            for language_index, language in enumerate(group):
+                if ((group_index - source_group_index + language_index - source_language_index) % source_group_size == 0) != (group_index == source_group_index):
+                    yield language
+
+
+ARCH = "hard"
+
+source_langs = [(None if lang == "none" else lang) for lang in sys.argv[2:]]
+if "all" in source_langs:
+    source_langs = flatten(language_groups)
+
+
+def main():
+    mt = train.Multitrainer(langs=set(flatten(language_groups)), pretrain_epochs=10, train_epochs=20, arch=ARCH)
+
+    for lang in flatten(language_groups):
+        for suffix in ["train-high", "train-medium", "dev", "test"]:
+            f = f"conll2018/task1/all/{lang}-{suffix}"
+            if not os.path.exists(f):
+                print(f, "does not exist")
+
+    pairs = []
+    for source_lang in source_langs:
+        for target_lang in get_target_languages(source_lang):
+            mt.train_pair(source_lang, target_lang)
+            sigmorphon19_task1_decode.main(ARCH, source_lang, target_lang, test=False)
+            correct_guesses, total_guesses = sigmorphon19_task1_decode.main(ARCH, source_lang, target_lang)
+            pairs.append((source_lang, target_lang, correct_guesses, total_guesses))
+
+    for source_lang, target_lang, correct_guesses, total_guesses in pairs:
+        print(f"{source_lang} -> {target_lang}: {correct_guesses}/{total_guesses} ({round(correct_guesses*100/total_guesses)}%)")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from code import WALS, unimorph, calculate, graphing, alignment, taginfo, fusion
+from code import WALS, unimorph, calculate, graphing, alignment, taginfo, fusion, modeling
 from crosslingual_inflection_baseline.src import train
 
 DISPATCH = {
@@ -11,6 +11,7 @@ DISPATCH = {
     "knowntags": taginfo.print_knowns,
     "calculate": calculate.calculate,
     "graph": graphing.graph,
+    "model": modeling.main
 }
 
 
@@ -21,6 +22,7 @@ def all():
     fusion.pull()
     calculate.calculate()
     graphing.graph()
+    modeling.main
 
 
 DISPATCH["all"] = all
@@ -31,8 +33,7 @@ if __name__ == "__main__":
         if command in DISPATCH:
             DISPATCH[command]()
         else:
-            # print(f"Unknown command: {command}")
-            train.main()
+            print(f"Unknown command: {command}")
 
     if len(sys.argv) == 1:
         print("Usage: python main.py <command>")
